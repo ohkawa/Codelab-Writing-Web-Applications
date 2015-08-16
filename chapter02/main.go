d2470e3b6d99f4ea485fc0aed1b2452dc7988c9a
ohkawa <ookawa.m@gmail.com>
2015-08-16 15:18:35 +0900

    create chapter02

---
+++ chapter02/main.go
@@ -0,0 +1,24 @@
+// Codelab: Webアプリケーションを書いてみよう - golang.jp
+// http://golang.jp/codelab-wiki
+//
+// サーバ立ち上げ
+// $ go run main.go
+//
+// ブラウザでアクセス
+// http://localhost:8080/
+
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
